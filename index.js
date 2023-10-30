d1a7d56778d157f65f458d688842c1306e5f3f3c
Jasmin Korat <jasminkorat204@gmail.com>
2023-10-30 17:07:42 +0530

    Type Number

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-let myAge:number = 21
-let pi:number =  3.14
-let a:number = 1
-
+"use strict";
+let myAge = 21;
+let pi = 3.14;
+let a = 1;
 // let b:number = a + "jasmin" //ERROR
 // console.log("b------------->",b)
-
 // let b:number = a  
 // console.log("b------------->",b)
-
-let b:number = a + pi
-console.log("b------------->",b)
+let b = a + pi;
+console.log("b------------->", b);
