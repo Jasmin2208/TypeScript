6417928fb0472464429fc635ff1bdcd17a86a6f8
Jasmin Korat <jasminkorat204@gmail.com>
2023-10-31 16:29:35 +0530

    Call Signature

---
+++ object/call_signatures.js
@@ -0,0 +1,9 @@
+"use strict";
+const teacher1 = {
+    name: "Komal Patel",
+    age: 29,
+    greet: (country) => `Country---------> ${country}`,
+    // state: (state: string) => `State---------> ${state}`
+};
+console.log(teacher1.greet("BHARAT"));
+// console.log(teacher1("Gujarat"));
