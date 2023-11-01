46e40c39c5747ff50c7f02fc8da1cf8285ad8a81
Jasmin Korat <jasminkorat204@gmail.com>
2023-11-01 11:19:59 +0530

    Intersection

---
+++ object/intersections.js
@@ -0,0 +1,13 @@
+"use strict";
+const employee = {
+    name: "jasmin korat",
+    age: 21,
+    emp_id: 1221,
+    department: "Web Developer"
+};
+console.log("employee----------->", employee);
+const emp = {
+    name: "jasmin korat",
+    age: 21
+};
+console.log("emp----------->", emp);
