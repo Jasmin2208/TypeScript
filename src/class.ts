29bbf701a3eefe7ec9af33a3f4a83cf23eef3f95
Jasmin Korat <jasminkorat204@gmail.com>
2023-11-01 17:09:19 +0530

    Class and Constructor

---
+++ src/class.ts
@@ -0,0 +1,17 @@
+class Person {
+    name: string;
+    age: number;
+    hobbies: string[];
+
+    constructor(name: string, age: number, hobbies: string[]) {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies
+    }
+}
+
+const person1: Person = new Person("jasmin", 21, ["chess", "travelling"])
+const person2: Person = new Person("happy", 23, ["chess", "travelling"])
+const person3: Person = new Person("vraj", 16, ["chess", "travelling"])
+
+console.log(person1, person2, person3)
