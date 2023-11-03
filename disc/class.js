0adf807aad793c5a9ab75df3f4b81cef7cbc8c38
Jasmin Korat <jasminkorat204@gmail.com>
2023-11-03 11:10:36 +0530

    static

--- disc/class.js
+++ disc/class.js
@@ -6,7 +6,9 @@ class Person {
         this.hobbies = hobbies;
     }
 }
+Person.mathPI = Math.PI;
 const person1 = new Person("jasmin", 21, ["chess", "travelling"]);
 const person2 = new Person("happy", 23, ["chess", "travelling"]);
 const person3 = new Person("vraj", 16, ["chess", "travelling"]);
 console.log(person1, person2, person3);
+console.log(Person.mathPI);
--- src/class.ts
+++ src/class.ts
@@ -8,6 +8,9 @@ class Person {
         this.age = age;
         this.hobbies = hobbies
     }
+
+    public static mathPI = Math.PI
+
 }
 
 const person1: Person = new Person("jasmin", 21, ["chess", "travelling"])
@@ -15,3 +18,4 @@ const person2: Person = new Person("happy", 23, ["chess", "travelling"])
 const person3: Person = new Person("vraj", 16, ["chess", "travelling"])
 
 console.log(person1, person2, person3)
+console.log(Person.mathPI)
